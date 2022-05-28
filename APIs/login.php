2ec04f859b2158dadfc04cdde6da6e58eec6bb44
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-28 23:18:42 +0300

    adding the login API PHP

---
+++ APIs/login.php
@@ -0,0 +1,22 @@
+<?php
+include("connection.php");
+$email = $_POST["email"];
+$password = hash("sha256", $_POST["password"]);
+$query = $mysqli->prepare("Select user_id from users where email = ? AND password = ?");
+$query->bind_param("ss", $email, $password);
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows;
+$query->bind_result($id);
+$fetched = $query->fetch();
+echo $fetched;
+$response = [];
+if($num_rows == 0){
+    $response["response"] = "User Not Found";
+}else{
+    $response["response"] = "Logged in";
+    $response["user_id"] = $id;
+}
+$json = json_encode($response);
+echo $json;
+?>
