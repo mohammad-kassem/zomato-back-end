4928a69b50ecbc03d769ee311fc3e61d480932d0
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-29 12:02:26 +0300

    adding the display users (admin) API PHP

---
+++ APIs/admin-display-users.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+  
+include("connection.php");
+
+$query = $mysqli->prepare("SELECT * FROM users");
+$query->execute();
+$array = $query->get_result();
+$response = [];
+while($user = $array->fetch_assoc()){
+    $response[] = $user;
+} 
+
+$json = json_encode($response);
+echo $json;
+
+?>
