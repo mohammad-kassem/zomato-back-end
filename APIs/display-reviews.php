f041218de0a2c39fdbd3d53f31eabd2df069269a
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-29 15:19:13 +0300

    adding the display restaurant reviews API PHP

---
+++ APIs/display-reviews.php
@@ -0,0 +1,14 @@
+<?php
+include("connection.php");
+$restaurant_id = $_GET["restaurant_id"];
+$query=$mysqli->prepare("Select concat(u.first_name, u.last_name), re.content, re.rating, re.date_posted from users u, reviews re where re.restaurant_id = ? and u.user_id = re.user_id");
+$query->bind_param("i", $restaurant_id);
+$query->execute();
+$array= $query->get_result();
+$response = [];
+while ($row = $array->fetch_assoc()){
+    $response[] = $row;
+}
+$json = json_encode($response);
+echo $json;
+?>
