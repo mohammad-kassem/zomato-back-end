59e28ba18bb5f4894a75745dcf71468ca126087d
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-29 17:18:15 +0300

    adding the adding a restaurant review API PHP

---
+++ APIs/review-restaurant.php
@@ -0,0 +1,14 @@
+<?php
+include("connection.php");
+$user_id = $_POST["user_id"];
+$restaurant_id = $_POST["restaurant_id"];
+$rating = $_POST["rating"];
+$content = $_POST["content"];
+$status = 0;
+$query = $mysqli->prepare("Insert into reviews (user_id, restaurant_id, rating, content, status, date_posted) VALUES (?, ?, ?, ?, ?, NOW()");
+$query->bind_param("iiisis", $user_id, $restaurant_id, $rating, $content, $status);
+$query->execute();
+$response["response"] = "Review added successfully";
+$json = json_encode($response);
+echo $json;
+?>
