673c977cb6cd8ed653a9302b8c4ac6f95a2d3858
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-31 22:57:02 +0300

    fixing some bugs

--- APIs/get-profile-details.php
+++ APIs/get-profile-details.php
@@ -2,7 +2,7 @@
 include("connection.php");
 if(isset($_GET["user_id"])) $user_id = $_GET["user_id"];
 else die("Please log in");
-$query = $mysqli->prepare("Select concat(first_name, ' ', last_name) as full_name, first_name, last_name, dob, gender, location from users where user_id = ?");
+$query = $mysqli->prepare("Select concat(first_name, ' ', last_name) as full_name, first_name, last_name, dob, gender, location as user_location from users where user_id = ?");
 $query->bind_param("i", $user_id);
 $query->execute();
 $user_details = $query->get_result();
