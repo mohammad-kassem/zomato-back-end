4dd77ac2822186d0a0391e0f0d8543d2d902a4ed
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-01 00:58:23 +0300

    minor fixes

--- APIs/display-restaurants.php
+++ APIs/display-restaurants.php
@@ -2,7 +2,7 @@
 include("connection.php");
 if(isset($_GET["user_id"])) $user_id = $_GET["user_id"];
 else die("Please log in");
-$query=$mysqli->prepare("Select r.restaurant_id, r.restaurant_name, r.image, avg(re.rating) as rating from restaurants r left join reviews re on r.restaurant_id = re.restaurant_id group by r.restaurant_id");
+$query=$mysqli->prepare("Select r.restaurant_id, r.restaurant_name, location, r.image, avg(re.rating) as rating, c.category_name from restaurants r left join reviews re on r.restaurant_id = re.restaurant_id join categories c on c.category_id=r.category_id group by r.restaurant_id");
 $query->execute();
 $array = $query->get_result();
 $response =[];
--- APIs/retaurant-details.php
+++ APIs/retaurant-details.php
@@ -4,7 +4,7 @@ if(isset($_GET["user_id"])) $user_id = $_GET["user_id"];
 else die("Please log in");
 if(isset($_GET["restaurant_id"])) $restaurant_id = $_GET["restaurant_id"];
 else die("Please specify a restaurant");
-$query=$mysqli->prepare("Select r.restaurant_name, r.description as restaurant_description, r.image, r.location, r.average_cost, avg(re.rating) as restaurant_rating from restaurants r, reviews re where r.restaurant_id = ? and re.restaurant_id = ?");
+$query=$mysqli->prepare("Select r.restaurant_name, r.description as restaurant_description, r.image, r.location, r.average_cost, avg(re.rating) as restaurant_rating from restaurants r, reviews re, categories c where r.restaurant_id = ? and re.restaurant_id=?");
 $query->bind_param("ii", $restaurant_id, $restaurant_id);
 $query->execute();
 $array= $query->get_result();
